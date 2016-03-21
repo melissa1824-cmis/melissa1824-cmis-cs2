9f5986eccefd59af2b8a73c14cd656c6c13c9051
cmis <cmis@cmis-hs21.(none)>
2016-03-21 10:55:20 +0700

    started

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+#My game will be like Monopoly. You choose a city for yourself and your opponent. Each city has a different value (random value generated = how much the city is worth in dollars). If you choose a city with a higher value, you win. If not, you lose. 
+
+def choose(city):
+    if city == "New York":
+        strength = int(10,000,000,000)
+    elif city == "Los Angeles" 
+        strength = int(90,000,000,000)
+    elif city == "New Orleans"
+        strength = int(80,000,000,000)
+    elif city
