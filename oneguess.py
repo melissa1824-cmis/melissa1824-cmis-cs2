8f73ead182c0e6aa7e4fbde73c335303f47df863
cmis <cmis@cmis-hs21.(none)>
2016-03-21 10:10:51 +0700

    Finished

--- oneguess.py
+++ oneguess.py
@@ -4,7 +4,7 @@ import math
 def output(number1, number2,):
     return """
 I'm thinking of a number from {} to {}.
-""".format(number1, number2)
+""". format(number1, number2)
 
 def result(target, guess, offby):
     if target > guess:
@@ -12,27 +12,27 @@ def result(target, guess, offby):
 The target was {}.
 Your guess was {}.
 That's under by {}.
-""".format(target, guess, offby)
+""". format(target, guess, offby)
 
     elif target == guess:
         print """
 The target was {}.
 Your guess was {}.
 You're a mind reader!
-""".format(target, guess, offby)
+""". format(target, guess, offby)
     
     else:
-        print """
+            print """
 The target was {}.
 Your guess was {}.
 That's over by {}.
-""".format(target, guess, offby)
+""". format(target, guess, offby)
     
 def main():
-    number1 = raw_input("What is the minimum number? ")
-    number2 = raw_input("What is the maximum number? ")
+    number1 = int(raw_input("What is the minimum number? "))
+    number2 = int(raw_input("What is the maximum number? "))
     print output(number1, number2)
-    guess = raw_input("What do you think it is? ")
+    guess = int(raw_input("What do you think it is? "))
     target = random.randint(int(number1), int(number2)) 
     offby = abs(int(target) - int(guess))
     result(target, guess, offby)
