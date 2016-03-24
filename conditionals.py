3027c28266499e66d3a99f8323058133a8a33253
cmis <cmis@cmis-hs21.(none)>
2016-03-24 08:30:35 +0700

    I'm going to cry

--- conditionals.py
+++ conditionals.py
@@ -1,56 +1,83 @@
 import random
 
-#My game will be like Monopoly. You choose a city from the list for yourself and your opponent. Each city has a different value (random value generated = how much the city is worth in dollars). After choosing a city, a tax value is subtracted from the value of the city. This tax value is randomly generated. If the final value you have is higher than the opponent's, you win. If not, you lose. *** Happiness of people is important. *** 
+#My game will be like Monopoly. You choose a city from the list for yourself and your opponent. Each city has a different value (random value generated = how much the city is worth in dollars). After choosing a city, a tax value is subtracted from the value of the city. This tax value is randomly generated. If the final value you have is higher than the opponent's, you win. If not, you lose. *** Happiness of citizens is important. *** 
 
 def value(city):
     if city == "Washington":
-        value = int(100,000,000)
+        value = 100,000,000
     if city == "New York":
-        value = int(90,000,000)
+        value = 90,000,000
     elif city == "San Francisco": 
-        value = int(80,000,000)
+        value = 80,000,000
     elif city == "Houston":
-        value = int(70,000,000,000)
+        value = 70,000,000,000
     elif city == "Los Angeles":
-        value = int(60,000,000)
+        value = 60,000,000
     elif city == "Las Vegas":
-        value = int(50,000,000)
+        value = 50,000,000
     elif city == "Detroit":
-        value = int(40,000,000)
+        value = 40,000,000
     elif city == "New Orleans":
-        value = int(30,000,000)
+        value = 30,000,000
     elif city == "San Jose":
-        value = int(20,000,000)
+        value = 20,000,000
     elif city == "Philadelphia":
-        value = int(10,000,000)
+        value = 10,000,000
     else:
-        strength = random.randint(int(10,000,000), int(100,000,000))
+        value = random.randint((10,000,000), (100,000,000))
     return value
 
 def taxValue():
     tax = int(raw_input("What tax value do you desire (10,000,000-100,000,000): "))
-    if tax > 100,000,000 or tax < 10,000,000:
+    if tax > int(100,000,000) or tax < int(10,000,000):
         tax = random.randint(10,000,000, 100,000,000)
     taxValue = float(tax) * 5
     return taxValue
 
-def citizens(value, taxValue):
+def citizens(playerValue, enemyValue, taxValue):
     population = random.random()
     happiness1 = (value + taxValue ** 3) / population
-    happiness2 = (value + taxValue ** 3) / population
-    if happiness1 > happiness2
+    happiness2 = (enemyValue + taxValue ** 3) / population
+    return 
+
+def total(playerValue, taxValue, citizens):
+    player1result = playerValue + tax + happiness1
+    player2result = value + tax + happiness2
+    return
+    if player1result > player2result:
         return True
-    else: 
+    else:
         return False
 
-def output(playerCity, enemyCity, taxValue):
-    return
-
+def result(playerValue, taxValue1, result1):
+    if result == True:
+        msg = "You won! Your city is worth more!"
+    else:
+        msg = "You lost :( Your opponent's city is worth more."
+    return """
+Your city's total value: {}
+Your opponent's city's total value: {}
+""".format(playerValue, taxValue1, result1)
 
 def main():
     playerCity = raw_input("Which city do you choose?: ")
     enemyCity = raw_input("Which city does enemy 1 choose?: ")
 
-    
+    playerValue = value(playerCity)
+    enemyValue = value(enemyCity)
 
-    
+    player1tax = taxValue()
+    result1 = total(playerValue, player1tax, citizens)
+
+    player2tax = taxValue()
+    result2 = total(playerValue, player2tax, citizens)
+
+    output = """
+Your city is {}.
+Your opponent's city is {}.
+""".format(playerCity, enemyCity)
+    output += result(playerValue, taxValue, total1)
+    output += result(playerValue, taxValue, total2)
+
+    print output
+main()
