3f33f07331345763cf2431384f811a9da3bd7ddd
cmis <cmis@cmis-hs01.(none)>
2016-04-26 08:41:12 +0700

    .

--- countup.py
+++ countup.py
@@ -1,12 +1,18 @@
+def countdown(a):
+    if a <= 0:
+        print "Blastoff!"
+    else:
+        print a
+        countdown(a-1)
+
 def countup(n):
-	if n >= 10:
+	if n >= 0:
 		print "Blastoff!"
 	else:
 	    print n
 	    countup(n+1)
 
 def main():
-    countup(6)
-    return
-
+    countdown(5)
+    countup(10)
 main()
