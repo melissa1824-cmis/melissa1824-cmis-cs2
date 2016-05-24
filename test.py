b41efd394c637e81349eeeb3ec36971e8357a647
cmis <cmis@cmis-hs16.(none)>
2016-05-24 13:33:42 +0700

    I feel so smart

---
+++ test.py
@@ -0,0 +1,37 @@
+#while 3 > 2:
+    #print ":)"
+
+#x = 0
+#while x < 10:
+    #print x
+    #x += 1
+
+#x = 10
+#while x >= 0:
+    #print x
+    #x -= 1
+
+#def countdown(x):
+    #while x >= 0:
+        #print x
+        #x -= 1
+
+#def countup(x):
+    #while x <= 10:
+        #print x
+        #x += 1
+
+def count(x):
+    while x > 0:
+        print x
+        x -= 1
+    while x < 0:
+        print x
+        x += 1
+
+#countdown(7)
+#countup(-6)
+
+count(-100)
+count(100)
+
