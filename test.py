14cf3100acf163c2a8b868088adc014fae60cdad
cmis <cmis@cmis-hs21.(none)>
2016-05-26 08:42:36 +0700

    stuff

--- test.py
+++ test.py
@@ -21,17 +21,43 @@
         #print x
         #x += 1
 
-def count(x):
-    while x > 0:
-        print x
-        x -= 1
-    while x < 0:
-        print x
-        x += 1
+#def count(x):
+    #while x > 0:
+        #print x1
+        #x -= 1
+    #while x < 0:
+        #print x
+        #x += 1
+
+#def countFrom2(x, y):
+    #while x < y:
+        #print x
+        #x += 1
+    #while x >= y:
+        #print x
+        #x -= 1
+
+def addodds(n):
+    if n < 0:
+        while n % 2 == 1:
+            n += 1
+            return sum
+        print sum(n)
+    elif n > 0:
+        while n % 2 != 1:
+            n -= 1
+            return sum
+        print sum(n)
+
 
 #countdown(7)
 #countup(-6)
 
-count(-100)
-count(100)
+#count(-100)
+#count(100)
+
+#countFrom2(-100, 0)
+#countFrom2(1, 100)
+
+addodds(-10)
 
